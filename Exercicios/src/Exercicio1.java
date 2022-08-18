38d3ff874e3cca584571a8568eabaa496a35273e
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 15:58:34 -0300

    exercicio 1

---
+++ Exercicios/src/Exercicio1.java
@@ -0,0 +1,13 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        
+        // EXERCICIO EM SALA DE AULA 1
+        // IMPRIMA TODOS OS NUMEROS DE 150 A 300
+        
+        //criando laço de repetição com as condições pedidas
+        for(int i = 150 ; i <= 300; i++){
+            System.out.println(i);  // imprimindo numeros inteiros de 150 ate 300
+        }
+
+    }
+}
