7099eb87ba1bc4536f07b179b029abc691b82f57
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 16:46:35 -0300

    gg

---
+++ Exercicios/src/Exercicio4.java
@@ -0,0 +1,14 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        
+        // EXERCICIO EM SALA DE AULA 4
+        //IMPRIMA TODOS OS FATORIAIS DE 1 A 10
+
+        int fatorial = 1;
+
+        for(int i = 1; i <= 10; i++){
+            fatorial = fatorial * i;
+            System.out.println(fatorial);
+        }
+    }
+}
