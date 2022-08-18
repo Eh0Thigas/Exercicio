5a160b3470ee3a09585bc5833688d04c5817b620
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 16:21:42 -0300

    dd

--- Exercicios/src/Exercicio2.java
+++ Exercicios/src/Exercicio2.java
@@ -3,10 +3,17 @@ public class Exercicio2 {
         
         // EXERCICIO EM SALA DE AULA 2
         // IMPRIMA A SOMA DE 1 ATÉ 1000
-
+        // Inicializando variavies 
         int i = 0;
+        int NumeroAnterior = 0;
+        int soma = 0;
+        
         while(i <=1000){
-            
+            NumeroAnterior = soma; // numeroanterior recebe soma
+            soma = i + NumeroAnterior;
+
+            System.out.println(NumeroAnterior + " + " + i + " = " + soma);
+            i++;
         }
     }
 }
