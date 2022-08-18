bd5ec7a84057a4e1d2984925c75a5d8da4769be9
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 16:10:45 -0300

    hh

---
+++ Exercicios/src/Exercicio2.java
@@ -0,0 +1,12 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        
+        // EXERCICIO EM SALA DE AULA 2
+        // IMPRIMA A SOMA DE 1 ATÉ 1000
+
+        int i = 0;
+        while(i <=1000){
+            
+        }
+    }
+}
