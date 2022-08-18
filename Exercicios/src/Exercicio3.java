dbf0e20ea46e2a21532519b16a3b003dfb767691
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 16:38:07 -0300

    dd

--- Exercicios/src/Exercicio3.java
+++ Exercicios/src/Exercicio3.java
@@ -2,7 +2,24 @@ public class Exercicio3 {
     public static void main(String[] args) {
         // EXERCICIO EM SALA DE AULA 3
         //IMPRIMA TODOS OS NUMEROS MULTIPLOS DE 3, ENTRE 1 A 100
-
         
+        int multiplo = 0;
+
+        for(int i = 1; i <=100; i++){
+            if(multiplo > 100){
+                break;
+            }else{
+                multiplo = i * 3;
+                System.out.println(multiplo);
+            }
+        }
+
+        // outro exemplo 
+        System.out.println("\n");
+        for(int i = 0; i <=100; i++){
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+        }
     }
 }
