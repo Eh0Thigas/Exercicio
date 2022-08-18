8d0cee79842103c13062dd42a0f2764d5bf9392b
Thiago <thiagolaurenti123@gmail.com>
2022-08-18 16:28:47 -0300

    dd

---
+++ Exercicios/src/Exercicio3.java
@@ -0,0 +1,8 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        // EXERCICIO EM SALA DE AULA 3
+        //IMPRIMA TODOS OS NUMEROS MULTIPLOS DE 3, ENTRE 1 A 100
+
+        
+    }
+}
