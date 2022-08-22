bf2f08f723e2a381836c4a242d7b64b6dac3fc20
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 13:53:49 -0300

    dd

--- Exercicios18_08/src/Exercicio6.java
+++ Exercicios18_08/src/Exercicio6.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Exercicio6 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int x;
 
         System.out.println("Digite um valor pra X");
-        x = s.nextInt();
+        x = sc.nextInt();
 
         if(x%2 == 0){
             System.out.println(x + " / 2  = ");
