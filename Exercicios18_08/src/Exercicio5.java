cd2983fad3d6343a299787d2608955afc7ea4f8f
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 13:53:08 -0300

    dd

---
+++ Exercicios18_08/src/Exercicio5.java
@@ -0,0 +1,16 @@
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        
+        int n1 = 1 , n2 = 0, n;
+
+        for(int i = 0; i <=100;i++){
+            n = n1 + n2;
+            n2 = n1;
+            System.out.println(n);
+        }
+
+        
+
+    }
+}
