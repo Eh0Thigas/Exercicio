3396527f32255a8eb1e70213f5f4f36517d68e2c
Thiago <thiagolaurenti123@gmail.com>
2022-08-22 13:57:28 -0300

    gg

--- Exercicios18_08/src/Exercicio5.java
+++ Exercicios18_08/src/Exercicio5.java
@@ -2,15 +2,19 @@
 public class Exercicio5 {
     public static void main(String[] args) {
         
-        int n1 = 1 , n2 = 0, n;
-
-        for(int i = 0; i <=100;i++){
-            n = n1 + n2;
-            n2 = n1;
-            System.out.println(n);
-        }
-
+       int num1 = 1, num2 = 0;
         
+        System.out.println(num2);
+        System.out.println(num1);
+        
+        for(int i = 0; i < 14; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
+            if (num1 >= 100){
+                break;
+            }
+        }
 
     }
 }
